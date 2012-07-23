4a86ece2f6d54de500a9fa22dc6454d2c9d18495
atchan <anitatchan@gmail.com>
2012-07-23 15:57:05 -0700

    yay

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -5,16 +5,17 @@ function MusuWriter(app) {
 var musu;
 Musubi.ready(function(context) {
     musu = new MusuWriter(context);
+    
+    //this code isn't working
 
 	$("#post_button").click(function(e) {
-		var style = "font-size:30px;padding:5px;";
+		var style = "font-size:30px; padding:5px;";
     	var text1 = "Started a Nose Goes. Here is the message - "
     	var text = $("#text_area").val();
     	var text2 = " - Hurry and join in if you don't want to lose!"
-    	var html = '<span style="' + style + '">' text1 + text + text2 '</span>';
+    	var html = '<span style="' + style + '">' + text1 + text + text2 + '</span>';
     	var content = { "__html" : html, "text" : text};
       	var obj = new SocialKit.Obj({type : "game_start", json: content})
       	musu.appContext.feed.post(obj);
- 		//musu.appContext.quit();
     }); 
 });	
