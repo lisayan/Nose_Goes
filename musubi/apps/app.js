cae7b260031b3364b0f427a5a7392125c1feca48
atchan <anitatchan@gmail.com>
2012-07-23 15:35:40 -0700

    shit

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -7,8 +7,8 @@ Musubi.ready(function(context) {
     musu = new MusuWriter(context);
 
 	$("#post_button").click(function(e) {
-    	var text = "POOOOOOOOO";
-    	var html = text;
+    	var text = "POOOOOOOOO"; //$("#text_area").val()
+    	var html = '<span>' + text + '</span>';
     	var content = { "__html" : html, "text" : text};
       	var obj = new SocialKit.Obj({type : "game_start", json: content})
       	musu.appContext.feed.post(obj);
