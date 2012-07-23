80dbfe67e915b291026c426fa893d1029abb899e
atchan <anitatchan@gmail.com>
2012-07-23 12:36:03 -0700

    slkdjf

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -6,10 +6,9 @@ var musu;
 Musubi.ready(function(context) {
     musu = new MusuWriter(context);
 
-	$("#post_button").click(function(e) {	
-		var style = "font-size:16px;";
+	$("#post_button").click(function(e) {
     	var text = $("#text_area").val();
-    	var html = '<span style="' + style + '">' + text + '</span>';
+    	var html = '<span>' + text + '</span>';
     	var content = { "__html" : html, "text" : text};
       	var obj = new SocialKit.Obj({type : "game_start", json: content})
       	musu.appContext.feed.post(obj);
