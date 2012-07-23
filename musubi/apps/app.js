f21ada5cd9936d2ce2577da19c51298a0282f243
atchan <anitatchan@gmail.com>
2012-07-23 12:23:41 -0700

    shit

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -16,7 +16,8 @@ Musubi.ready(function(context) {
  		//musu.appContext.quit();
     }); 
     
-    $("#the_nose").click(function(e) {
+    $(function success(e) {
+    	var style = "font-size:16px;";
     	var text = "Pressed the nose!";
     	var html = '<span style="' + style + '">' + text + '</span>';
     	var content = { "__html" : html, "text" : text};
