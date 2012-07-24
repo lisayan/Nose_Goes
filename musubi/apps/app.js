507f080adf9543fccdb405362f029028d4bcda3b
atchan <anitatchan@gmail.com>
2012-07-24 12:34:12 -0700

    ;alskdjf

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -9,11 +9,11 @@ Musubi.ready(function(context) {
     //this code isn't working
 
 	$("#post_button").click(function(e) {
-		var style = "font-size:30px; padding:5px;";
     	var text1 = "Started a Nose Goes. Here is the message - "
     	var text = $("#text_area").val();
     	var text2 = " - Hurry and join in if you don't want to lose!"
-    	var html = '<span style="' + style + '">' + text1 + text + text2 + '</span>';
+    	var text3 = "<img src='http://lisayan.github.com/Nose_Goes/musubi/apps/images/nose_goes_icon.png'>";
+    	var html = text1 + text + text2 + text3;
     	var content = { "__html" : html, "text" : text};
       	var obj = new SocialKit.Obj({type : "game_start", json: content})
       	musu.appContext.feed.post(obj);
