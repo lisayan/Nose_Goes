5876151cc46aefc47030f689dfe38465e5ba6d13
atchan <anitatchan@gmail.com>
2012-07-24 12:51:57 -0700

    update

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -12,10 +12,19 @@ Musubi.ready(function(context) {
     	var text1 = "Started a Nose Goes. Here is the message - "
     	var text = $("#text_area").val();
     	var text2 = " - Hurry and join in if you don't want to lose!"
-    	var text3 = "<img src='http://lisayan.github.com/Nose_Goes/musubi/apps/images/nose_goes_icon.png'>";
-    	var html = text1 + text + text2 + text3;
+    	var html = text1 + text + text2;
     	var content = { "__html" : html, "text" : text};
       	var obj = new SocialKit.Obj({type : "game_start", json: content})
       	musu.appContext.feed.post(obj);
     }); 
+
+    $("#the_nose").click(function(e) {
+    	var text = "Pressed the nose!";
+    	var text2 = "<img src='http://lisayan.github.com/Nose_Goes/musubi/apps/images/nose_goes_icon.png'>";
+    	var html = text + text2;
+    	var content = { "__html" : html, "text" : text};
+      	var obj = new SocialKit.Obj({type : "post_user", json: content})
+      	musu.appContext.feed.post(obj);
+      	musu.appContext.quit();
+	});
 });	
