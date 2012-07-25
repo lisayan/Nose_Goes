c181f56e4e228fa312f9ad2f5b57ccc808f48f62
atchan <anitatchan@gmail.com>
2012-07-25 11:20:40 -0700

    test

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -9,7 +9,19 @@ function foo() {
 var musu;
 Musubi.ready(function(context) {
     musu = new MusuWriter(context);
+    
     alert("musubi is ready");
+    
+    $("#start_button").click(function(e) {
+    	var text1 = "Started a Nose Goes. Here is the message - ";
+    	var text = $("#text_area").val();
+    	var text2 = " - Hurry and join in if you don't want to lose!";
+    	var html = text1 + text + text2;
+    	var content = { "__html" : html, "text" : text};
+      	var obj = new SocialKit.Obj({type : "nose_goes_state", json: content});
+      	musu.appContext.feed.post(obj); //post message for game start
+	});
+    
     $("#the_nose").click(function(e) {
     	/*var text = "Pressed the nose!";
     	var text2 = "<img src='http://lisayan.github.com/Nose_Goes/musubi/apps/images/nose_goes_icon.png'>";
